c9fe3ab8e290734439fb50c910b2a8bd35b18d38
lisakolts <crokodile.liza@gmail.com>
2024-09-30 15:42:05 +0300

    Основа для слайдера

---
+++ src/components/banner-item/banner-item.tsx
@@ -0,0 +1,32 @@
+import { Promo } from '../../types/promo';
+
+type BannerItemProps ={
+  promo: Promo;
+}
+
+export default function BannerItem({promo}: BannerItemProps): JSX.Element {
+  const {
+    name,
+    previewImg,
+    previewImg2x,
+    previewImgWebp,
+    previewImgWebp2x
+  } = promo;
+
+  return (
+    <div className="banner swiper-slide">
+      <picture>
+        <source type="image/webp" srcSet={`/${previewImgWebp}, /${previewImgWebp2x}`} />
+        <img src={`/${previewImg}`} srcSet={`/${previewImg2x} 2x`}width="1280" height="280" alt={name} />
+      </picture>
+      <p className="banner__info">
+        <span className="banner__message">
+          Новинка!
+        </span>
+        <span className="title title--h1">{name}</span>
+        <span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
+        <a className="btn" href="#">Подробнее</a>
+      </p>
+    </div>
+  );
+}
--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,11 +1,15 @@
+import { useAppSelector } from '../../hooks';
+import { getPromos } from '../../store/promo-data/selectors';
+import BannerItem from '../banner-item/banner-item';
+
 export default function Banner(): JSX.Element {
+  const promos = useAppSelector(getPromos);
+
   return (
-    <div className="banner">
-      <picture>
-        <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x" />
-        <img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер" />
-      </picture>
-      <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
+    <div className="swiper-wrapper">
+      {
+        promos.map((promo) => <BannerItem promo={promo} key={promo.name + promo.previewImg}/>)
+      }
     </div>
   );
 }
